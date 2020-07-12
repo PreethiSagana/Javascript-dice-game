c2cd3ab1de1a1672afdbd668e3666111ea393116
PreethiSagana <preethisagana16@gmail.com>
2020-07-12 16:32:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('.btn-roll').addEventListener('click', function() {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+        }  
+});
